cc1853368f91b37a30e9b587f10eb6580e02bc06
Nicolas Cumez <ncumez.mingob@gmail.com>
2017-04-26 10:41:05 -0600

    Inicialización del proyecto con Framework Laravel

---
+++ config/constantes.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Archivo de constantes de uso general en el sistema
+ */
+
+return array(
+    'sistema' => 'sisTaller',
+    'datatableListRows' => "[[5,20, 50, 100],[5,20, 50, 100]]",
+);
