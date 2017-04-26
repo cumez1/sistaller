2adc405bdae214a3051d268530ecaa85fe8510d3
Nicolas Cumez <ncumez.mingob@gmail.com>
2017-04-26 17:32:35 -0600

    Actualización del fron-end

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,15 +1,13 @@
 @extends('layouts.auth')
 
-@section('htmlheader_title')
-    Password recovery
-@endsection
+@section('htmlheader_title','Recuperar Contraseña')
 
 @section('content')
 
 <body class="login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="{{ url('/home') }}"><b>Admin</b>LTE</a>
+            <a href="{{ url('/home') }}"><b>Sistemas</b>GT</a>
         </div><!-- /.login-logo -->
 
         @if (session('status'))
